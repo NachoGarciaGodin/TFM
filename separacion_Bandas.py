cc679521b88b1e761a39f78bd8f3664a3f64f7cb
Ignacio Garcia <167064063+NachoGarciaGodin@users.noreply.github.com>
2024-10-12 13:47:01 +0200

    Add files via upload

--- separacion_Bandas.py
+++ separacion_Bandas.py
@@ -17,13 +17,13 @@ def guardar_banda(input_tif, output_tif, band_number):
 input_tif = "C:/Users/nacho/OneDrive - Universidad de Alcala/TFM/imagenes/812T.tif"
 
 # Guardar las bandas 6, 7 y 8 en archivos separados
-guardar_banda(input_tif, '812T_banda2.tif', 1)
-guardar_banda(input_tif, '812T_banda3.tif', 2)
-guardar_banda(input_tif, '812T_banda4.tif', 3)
-guardar_banda(input_tif, '812T_banda5.tif', 4)
-guardar_banda(input_tif, '812T_banda6.tif', 5)
-guardar_banda(input_tif, '812T_banda7.tif', 6)
-guardar_banda(input_tif, '812T_banda8.tif', 7)
+guardar_banda(input_tif, '812T_banda1.tif', 1)
+guardar_banda(input_tif, '812T_banda2.tif', 2)
+guardar_banda(input_tif, '812T_banda3.tif', 3)
+guardar_banda(input_tif, '812T_banda4.tif', 4)
+guardar_banda(input_tif, '812T_banda5.tif', 5)
+guardar_banda(input_tif, '812T_banda6.tif', 6)
+guardar_banda(input_tif, '812T_banda7.tif', 7)
 guardar_banda(input_tif, '812T_banda8A.tif', 8)
 guardar_banda(input_tif, '812T_banda9.tif', 9)
 guardar_banda(input_tif, '812T_banda10.tif', 10)
